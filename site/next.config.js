ac8babc1a7702ed5e154ac1774e6079df61213fe
sowwai-kok <sowwai.kok@arkstack.io>
2024-11-12 15:44:26 +0800

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
     name: 'Kasm Technologies',
     description: 'The official store for Kasm supported workspaces.',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
+    listUrl: 'https://sowwai-kok.github.io/kasm-ecr/',
     contactUrl: 'https://kasmweb.com/support',
   },
   reactStrictMode: true,
